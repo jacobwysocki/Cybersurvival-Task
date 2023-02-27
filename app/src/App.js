b5cc6630733a7c6d904b98bca1180c1e0b35bb8f
Jakub <kubawysocki00@gmail.com>
2023-02-27 18:19:53 +0000

    Simple homepage with links to log in and register forms, added react-router-dom and react-bootstrap.

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {Routes, Route} from 'react-router-dom';
+
+import React, {useState, useEffect} from 'react';
+
+import HomePage from './components/HomePage';
+import Login from "./components/Login";
+
 function App() {
+
+
+  const [authenticated, setAuthenticated] = useState(false);
+  const handleAuthenticated = (isAuthenticated) => {
+    setAuthenticated(isAuthenticated)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<Login handleAuthenticated={setAuthenticated}/>}/>
+          <Route path="*" element={<p>Not Found</p>}/>
+        </Routes>
       </header>
     </div>
   );
